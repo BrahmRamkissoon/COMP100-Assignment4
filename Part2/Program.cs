e006061506c81fbf7066468ee09d04f80428bd52
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-14 19:37:03 -0400

    Part 2: added menu

--- Part2/Program.cs
+++ Part2/Program.cs
@@ -10,11 +10,45 @@ using System.Threading.Tasks;
 
 namespace Part2
 {
-    class Program
+    internal class Program
     {
-        static void Main ( string [] args )
+        private static void Main(string[] args)
         {
+            int prompt = 0; // Sentinel value for menu
 
+            while (prompt != 3)
+            {
+                // Display menu
+                Console.WriteLine( "++++ Reservation App ++++" );
+                Console.WriteLine( "+                       +" );
+                Console.WriteLine( "+    1. First Class     +" );
+                Console.WriteLine( "+    2. Economy         +" );
+                Console.WriteLine( "+    3. Exit            +" );
+                Console.WriteLine( "+                       +" );
+                Console.WriteLine( "+++++++++++++++++++++++++" );
+
+
+                Console.WriteLine( "++++++++++++++++++++++++++" );
+                Console.Write( "Please choose and option above (1 -3): " );
+
+                prompt = Convert.ToInt32( Console.ReadLine() );
+
+                switch (prompt)
+                {
+                    case 1:
+                        Console.WriteLine( "" );
+                        //PressKey();
+                        Console.Clear();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        Console.WriteLine( "Error" );
+                        break;
+                }
+            }
         }
     }
 }
