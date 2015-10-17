5b362c07a865bc702fcbdf7d2d67d0b32ac3586d
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-16 23:54:21 -0400

    part 1 updated frequency counter, removed switch

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -18,67 +18,32 @@ namespace Assignment4
 
             Random randomRoll = new Random(); // random number generator
 
-            int[] frequency = new int[11]; // store frequency of possible sums, there are 12 possibilities
+            int[] frequency = new int[13]; // store frequency of sums (2 to 12) , ignore index 0 and 1
      
             int diceOne = 0;    // stores random roll of dice # 1
             int diceTwo = 0;    // stores random roll of dice # 2
             int sumOfDie = 0;   // sum of both dice
 
             // summarize results of count rolls of a die
-            for ( int roll = 0 ; roll <= COUNT ; roll++ )
+            for (int roll = 1; roll <= COUNT; ++roll)
             {
-                diceOne = 1 + randomRoll.Next(6); // randomly get a number from 1 to 6
-                diceTwo = 1 + randomRoll.Next( 6 );
+                diceOne = randomRoll.Next( 1, 7 ); // randomly get a number from 1 to 6
+                diceTwo = randomRoll.Next( 1, 7 );
                 sumOfDie = diceOne + diceTwo;
 
-                // determine roll value 2-12 and increment appropriate counter
-                switch ( sumOfDie )
-                {
-                    case 2:
-                        frequency[0] += 1; // increment the 2s counter
-                        break;
-                    case 3:
-                        frequency [1] += 1; // increment the 3s counter
-                        break;
-                    case 4:
-                        frequency [2] += 1; // increment the 4s counter
-                        break;
-                    case 5:
-                        frequency [3] += 1; // increment the 5s counter
-                        break;
-                    case 6:
-                        frequency [4] += 1; // increment the 6s counter
-                        break;
-                    case 7:
-                        frequency [5] += 1; // increment the 7s counter
-                        break;
-                    case 8:
-                        frequency [6] += 1; // increment the 8s counter
-                        break;
-                    case 9:
-                        frequency [7] += 1; // increment the 9s counter
-                        break;
-                    case 10:
-                        frequency [8] += 1; // increment the 10s counter
-                        break;
-                    case 11:
-                        frequency [9] += 1; // increment the 11s counter
-                        break;
-                    case 12:
-                        frequency [10] += 1; // increment the 12s counter
-                        break;
-                    
-                } // end switch
+
+                ++frequency[sumOfDie];      // increment  frequency[2] to frequency[12]
             } // end for
-            Console.WriteLine( "Sum of Both Die \tFrequency"); // output headers
+
+            // output table headers
+            Console.WriteLine( "Sum of Both Die \tFrequency"); 
             Console.WriteLine("+++++++++++++++++++++++++++++++++");
 
 
-            // display Results
-            for (int count = 0; count < frequency.Length; count++)
+            // display number of times frequency[2] to frequency[12] have been incremented
+            for (int count = 2; count < frequency.Length; ++count)
             {
-                int sumValue = frequency [count];
-                Console.WriteLine( $"\t{count+2}\t \t{sumValue}");
+                Console.WriteLine( $"\t{count}\t \t{frequency[count]}");
                 
             } // end for
 
